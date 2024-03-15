685d3311a4b1959139e71d474f5dbdd410d6a10b
James Milton <jamesxdigital@gmail.com>
2024-03-15 20:36:21 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ArScene from './components/ArScene';
+import HomePage from './components/HomePage'; // Import the HomePage component
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} /> {/* Set HomePage as the root route */}
         <Route path="/ar-image-1" element={
           <ArScene
             targetSrc="/target-image-1.jpg"
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Patent Machine AR</h1>
+      <p>This is the home page of your AR experience.</p>
+      {/* Add more content here as needed */}
+    </div>
+  );
+};
+
+export default HomePage;
