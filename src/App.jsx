a3f61b8569c5926eedfae09b39d678695354e7be
James Milton <jamesxdigital@gmail.com>
2024-03-15 20:44:41 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ArScene from './components/ArScene';
-import HomePage from './components/HomePage'; // Import the HomePage component
+import HomePage from './components/HomePage';
+import TestPage from './components/TestPage'; // Import the TestPage component
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} /> {/* Set HomePage as the root route */}
+        <Route path="/" element={<HomePage />} />
         <Route path="/ar-image-1" element={
           <ArScene
             targetSrc="/target-image-1.jpg"
             materialSrc="https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg"
           />
         }/>
-        {/* You can add more routes with different targets and materials */}
+        <Route path="/test" element={<TestPage />} /> {/* Add this line */}
       </Routes>
     </Router>
   );
